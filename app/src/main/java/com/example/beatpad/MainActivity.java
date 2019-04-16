7e6610a05a2d195bc6d07d2ed34e7698c096c62f
BrettPatterson1 <brettsp2@illinois.edu>
2019-04-16 16:52:18 -0500

    Added the layout, no logic implemented yet

--- app/src/main/java/com/example/beatpad/MainActivity.java
+++ app/src/main/java/com/example/beatpad/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.beatpad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pad1(View view) {
+
+    }
+
+    public void pad2(View view) {
+
+    }
+
+    public void pad3(View view) {
+
+    }
+
+    public void pad4(View view) {
+
+    }
+
+    public void pad5(View view) {
+
+    }
+
+    public void pad6(View view) {
+
+    }
+
+    public void pad7(View view) {
+
+    }
+
+    public void pad8(View view) {
+
+    }
+
+    public void pad9(View view) {
+
     }
 }
